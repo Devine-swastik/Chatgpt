9a98c4320ca58b4e0eca19e1c9885cf39903c6a2
Devine <devineparadoxz@gmail.com>
2024-10-19 23:48:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
 # Telegram Bot Configuration
-API_ID = int(os.getenv('API_ID', '29644708'))
+API_ID = int(os.getenv('API_ID', 29644708))
 API_HASH = os.getenv('API_HASH', '0db39046c635489ccb05d9a0ca395c9f')
 BOT_TOKEN = os.getenv('BOT_TOKEN', '7866262568:AAG-DzguR8kK6B7dzFQW53RFxoUUmeLzQsg')
 BOT_NAME = os.getenv('BOT_NAME', 'Chat Gpt')
 BOT_USERNAME = os.getenv('BOT_USERNAME', 'Devine_chatgpt_bot')
 OWNER_ID = int(os.getenv('OWNER_ID', '6338745050'))
 OWNER_USERNAME = os.getenv('OWNER_USERNAME', 'devine4x')
-UPDATE_CHANNEL = os.getenv('UPDATE_CHANNEL', '-1001835308211')
-SUPPORT_GROUP = os.getenv('SUPPORT_GROUP', '-1002082533215')
+UPDATE_CHANNEL = os.getenv('UPDATE_CHANNEL', 'Devine_Network')
+SUPPORT_GROUP = os.getenv('SUPPORT_GROUP', "Devine_Network")
 
 # OpenAI Configuration
 OPENAI_KEY = os.getenv('OPENAI_KEY', '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')
